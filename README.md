d591564857bb6128d69f2d7ee23bf20bab46c619
Vikas Yadav <yadavikas088@gmail.com>
2024-02-26 03:54:39 +0530

    (development): python code updated till model

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # django-learning
 Learning Python Framework Django
+
+# Learning #
+What is Django?
+1.Django is python web framework
+2.Django is backend server side web framework.
+3.Django makes easier to build web pages using python.
+   Take cares of difficult stuff so that we can concentrate on building web apps.
+4.Provides many built in features.
+
+# ===========================================================================
+
+MVT
+
+Django follows MVT design patterns:
+
+Model - data we want to present, usually data from a database
+View - A request handler that returns the relevant template and content - based on the request from the user.
+Template - A text file (like an HTML file) containing the layout of the web page, with logic on how to display the data.
+
+# ===========================================================================
+
+Set up the Environment
+Download and install python
+Check you pip is working
+Install Django
+	pip install Django
+	django-admin -version
+	django-admin startproject <projectname>
+	python manage.py startapp login
+	python manage.py runserver
+	python manage.py migrate
+	python manage.py makemigrations
+# ===========================================================================
+
+Lets Understand
+.Project structure
+.URLs					URLs Define
+.Views					functions & classes
+.Templates				HTML Templates
+.Models-CRUD OPERATIONS
+
+# ===========================================================================
+
---
+++ myapp/views.py
@@ -0,0 +1,64 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import datetime
+
+def home(request):
+
+    isActive = True
+    if(request.method=='POST'):
+        check = request.POST.get('check')
+        print(check)
+        if check is None: isActive = False
+        else: isActive = True
+
+    date = datetime.datetime.now()
+    
+    name = "Learning With Joy"
+    list_of_programs=[
+        'WAP to check even or odd',
+        'WAP to check prime number',
+        'WAP to print all prime numbers from 1 to 100',
+        'WAP to print pascals triangle'
+    ]
+    student={
+        'student_name': "Rahul",
+        'student_college': "XYZ",
+        'student_city': "MUMBAI"
+    }
+    print("test function is called from view")
+    # return HttpResponse("<h1>Hello this is index page </h1>"+str(date))
+    data={
+        'date': date,
+        'isActive': isActive,
+        'name': name,
+        'list_of_programs': list_of_programs,
+        'student_data': student
+    }
+    return  render(request, 'home.html', data) #{'current_time': str(date)},
+
+def about(request):
+    # return HttpResponse("<h1>This is about page</h1>")
+    return render(request,'about.html')
+    
+# def contact(request):
+#     if request.method == "POST":
+#         name=request.POST['name']
+#         email=request.POST['email']
+#         message=request.POST['message']
+            
+#         return HttpResponse('<h3>Name:{}<br/>Email:{},<br/>Message:{}</h3>'.format(name,email,message))
+#     else:
+#         return render(request,"contact.html")    
+
+def contact(request):
+    if request.method=="POST":
+        context={}
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data["subject"]
+            from_email = form.cleaned
+
+def services(request):
+    # return HttpResponse("<h1>This is services page</h1>")
+    return render(request,'services.html')    
